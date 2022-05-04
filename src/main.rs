2ea53509c469ccdace24718f43262237f5be0b5a
MisileLaboratory <icecreamhappytroll@gmail.com>
2022-05-04 04:05:33 +0000

    asd

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{BufReader, Result, Lines, BufRead};
+use std::io::{BufReader, BufRead};
 use std::path::Path;
 
 fn main() {
@@ -7,23 +7,41 @@ fn main() {
 }
 
 fn answerit() {
-    let pointinput = read_lines("pointinput.txt").unwrap();
-    let quespoint = read_lines("quespoint.txt").unwrap();
-    let quesanswer = read_lines("quesanswer.txt").unwrap();
-    let quesinput = read_lines("quesinput.txt").unwrap();
-    let point: u8 = 0;
+    let status = self_update::backends::github::Update::configure()
+    .repo_owner("jaemk")
+    .repo_name("self_update")
+    .bin_name("github")
+    .show_download_progress(true)
+    .current_version(cargo_crate_version!())
+    .build().unwrap()
+    .update().unwrap();
+    println!("Update status: `{}`!", status.version());
 
-    for (i, i2, i3) in (quespoint, quesanswer, quesinput) {
-        if i2 == i3 {
-            point = point + i
-        } else {
-            point = point - i
+    let quesanswer = read_lines("quesanswer.txt");
+    let quesinput = read_lines("quesinput.txt");
+    let quesbool = read_lines("quesbool.txt");
+    let mut falselist: Vec<usize> = Vec::new();
+
+    for i in 0..quesinput.len() {
+        println!("{}: {} - {}", quesinput[i], quesanswer[i], quesbool[i]);
+        if quesbool[i].parse::<bool>().unwrap() != (quesanswer[i] == quesinput[i]) {
+            falselist.push(i + 1);
         }
+    }
+
+    if falselist.len() == 0 {
+        println!("No wrong question!")
+    } else {
+        println!("Wrong question (or mistake input): {:#?}", falselist)
     }
 }
 
-fn read_lines<P>(filename: P) -> Result<Lines<BufReader<File>>>
+fn read_lines<P>(filename: P) -> Vec<String>
 where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(BufReader::new(file).lines())
+    let file = File::open(filename).unwrap();
+    let mut vector = vec![];
+    for i in BufReader::new(file).lines() {
+        vector.push(i.unwrap())
+    };
+    vector
 }
