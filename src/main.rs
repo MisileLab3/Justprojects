68590f92fb1675e5939608237074f04ebf1a883c
MisileLaboratory <icecreamhappytroll@gmail.com>
2022-05-09 03:55:44 +0000

    cli with clap

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,36 @@ use std::fs::File;
 use std::io::{BufReader, BufRead};
 use std::path::Path;
 
+use clap::Parser;
+
+/// Simple program for check this is false test point.
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+struct Arguments {
+    /// Question answer (only txt, line break)
+    #[clap(long)]
+    quesanswer: String,
+
+    /// Answer that you input to question (only txt, line break)
+    #[clap(long)]
+    quesinput: String,
+
+    /// That question input and answer is same? (only txt, line break)
+    #[clap(long)]
+    quesbool: String
+}
+
+
 fn main() {
     answerit();
 }
 
 fn answerit() {
-    let quesanswer = read_lines("quesanswer.txt");
-    let quesinput = read_lines("quesinput.txt");
-    let quesbool = read_lines("quesbool.txt");
+    let args = Arguments::parse();
+
+    let quesanswer = read_lines(args.quesanswer);
+    let quesinput = read_lines(args.quesinput);
+    let quesbool = read_lines(args.quesbool);
     let mut falselist: Vec<usize> = Vec::new();
 
     for i in 0..quesinput.len() {
